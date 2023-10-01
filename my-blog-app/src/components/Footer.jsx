67eef7ed36e6cc44ebfada1826559e2a601f40c3
Saumya Pandey <saumyaprashi@gmail.com>
2023-10-01 17:53:19 +0530

    First
    First

---
+++ my-blog-app/src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Logo from "../img/buzzblog-logo.png"
+
+const Footer = () => {
+  return (
+    <footer>
+      <img src={Logo} alt="" />
+      <span>
+        A work of art By <b>React.js</b>
+      </span>
+    </footer>
+  );
+};
+
+export default Footer
